7f7546936d1df04059179f2b905add9bc9931d0f
anton <sanghoemail@gmail.com>
2025-03-02 17:19:46 +0900

    로고를 눌렀을 때 나오도록하기

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ count = 1
 
 def on_forever():
     global count
-    strip.clear()
-    # count 개수만큼 LED 켜기
-    i = 0
-    while i <= count - 1:
-        strip.set_pixel_color(i, neopixel.colors(NeoPixelColors.RED))
-        i += 1
-    strip.show()
-    basic.pause(1000)
-    # LED 개수 증가시키기 (최대 8개까지)
-    count = count + 1
-    if count > 8:
-        count = 1
+    if input.logo_is_pressed():
+        strip.clear()
+        # count 개수만큼 LED 켜기
+        i = 0
+        while i <= count - 1:
+            strip.set_pixel_color(i, neopixel.colors(NeoPixelColors.RED))
+            i += 1
+        strip.show()
+        basic.pause(100)
+        # LED 개수 증가시키기 (최대 8개까지)
+        count = count + 1
+        if count > 8:
+            count = 1
+    else:
+        strip.clear()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,16 +4,20 @@ let count = 1
 // 처음에 켤 LED 개수
 // 최대 개수에 도달하면 다시 1개부터 시작
 basic.forever(function () {
-    strip.clear()
-    // count 개수만큼 LED 켜기
-    for (let i = 0; i <= count - 1; i++) {
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
-    }
-    strip.show()
-    basic.pause(100)
-    // LED 개수 증가시키기 (최대 8개까지)
-    count = count + 1
-    if (count > 8) {
-        count = 1
+    if (input.logoIsPressed()) {
+        strip.clear()
+        // count 개수만큼 LED 켜기
+        for (let i = 0; i <= count - 1; i++) {
+            strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
+        }
+        strip.show()
+        basic.pause(100)
+        // LED 개수 증가시키기 (최대 8개까지)
+        count = count + 1
+        if (count > 8) {
+            count = 1
+        }
+    } else {
+        strip.clear()
     }
 })
